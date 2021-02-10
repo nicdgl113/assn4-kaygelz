1282d51ef7adda1aee125bd702b78e956e618ce3
kaylee <70932714+kaygelz@users.noreply.github.com>
2021-02-09 16:22:45 -0800

    added window load event, moved greeting inside start function

--- home.js
+++ home.js
@@ -3,15 +3,23 @@
 //date:feb9
 //assn:4
  
+///window load event
+window.addEventListener("load", start);
+
+
+
 ///functions
+function start() {
 function greeting(guestName = "guest") {
     let htmlStr = "<p>Welcome, " + guestName + "!</p>";
     let myMain = document.querySelector("main");
-    myMain.innerHTML += htmlStr;
-    
+    myMain.innerHTML = htmlStr;
 }
+
+
 //call function
 ///greeting(); 
+
 let guest = prompt("tell me your name please");
 greeting(guest);
-
+}
